bc40121684361c0067d0968f44e2352a15dba2d6
Sergey Tarasov <sila080907@gmail.com>
2021-10-30 13:40:15 +0300

    add presentation layer, change list to livedata

--- app/src/main/java/com/tarasov/shoplist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/tarasov/shoplist/data/ShopListRepositoryImpl.kt
@@ -1,23 +1,35 @@
 package com.tarasov.shoplist.data
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.tarasov.shoplist.domain.ShopItem
 import com.tarasov.shoplist.domain.ShopListRepository
 import java.lang.RuntimeException
 
 object ShopListRepositoryImpl : ShopListRepository {
 
+    private val shopListLD = MutableLiveData<List<ShopItem>>()
     private val shopList = mutableListOf<ShopItem>()
     private var autoIncrementId = 0
+
+    init {
+        for (i in 0 until 10) {
+            val item = ShopItem("Name $i", i, true)
+            addShopItem(item)
+        }
+    }
 
     override fun addShopItem(shopItem: ShopItem) {
         if (shopItem.id == ShopItem.UNDEFINED_ID) {
             shopItem.id = autoIncrementId++
         }
         shopList.add(shopItem)
+        updateList()
     }
 
     override fun deleteShopItem(shopItem: ShopItem) {
         shopList.remove(shopItem)
+        updateList()
     }
 
     override fun editShopItem(shopItem: ShopItem) {
@@ -31,8 +43,12 @@ object ShopListRepositoryImpl : ShopListRepository {
             ?: throw RuntimeException("Element with id: $shopItemId not found")
     }
 
-    override fun getShopList(): List<ShopItem> {
-        return shopList.toList()
+    override fun getShopList(): LiveData<List<ShopItem>> {
+        return shopListLD
+    }
+
+    private fun updateList() {
+        shopListLD.value = shopList.toList()
     }
 
 }
--- app/src/main/java/com/tarasov/shoplist/presentation/MainActivity.kt
+++ app/src/main/java/com/tarasov/shoplist/presentation/MainActivity.kt
@@ -2,11 +2,26 @@ package com.tarasov.shoplist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
 import com.tarasov.shoplist.R
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+    private var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        viewModel.shopList.observe(this) {
+            Log.d("MainActivityTest", it.toString())
+            if (count == 0) {
+                count++
+                val item = it[0]
+                viewModel.changeEnableState(item)
+            }
+        }
     }
 }
